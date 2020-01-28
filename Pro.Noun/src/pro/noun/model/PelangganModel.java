39d007d6a9e376fb9835fec88913902b340ba303
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2020-01-28 19:54:41 +0700

    Add files via upload
    membuat Database perhitungan

---
+++ Pro.Noun/src/pro/noun/model/PelangganModel.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro.noun.model;
+
+/**
+ *
+ * @author asus
+ */
+public class PelangganModel {
+    
+    
+    private String nama;
+    private String email;
+    private String noTelp;
+    private String harga;
+    private String jumlah;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(String jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    
+    public void resetForm(){
+        
+        
+    }
+    
+    public void simpanForm(){
+        
+        
+    }
+    
+        
+}
